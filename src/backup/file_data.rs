02622e0c33aec5c6e3c77200fc03835ca512eedf
Lucas Liu <lucasliu7@hotmail.com>
2022-04-14 18:46:27 +1000

    created get backup state function

--- src/backup/file_data.rs
+++ src/backup/file_data.rs
@@ -6,50 +6,78 @@ use crate::config::config_types::GameConfig;
 
 use super::backup_types::BackupState;
 
-// pub fn get_backup_state(config: &GameConfig) -> Result<BackupState, io::Error> {
-//     // Check save files
-//     let root_path = PathBuf::from(&config.root);
-
-// }
-
-pub fn get_latest_backup_time(dir: &Path) -> Option<SystemTime> {
-    let dir_exists = dir.is_dir();
-    if !dir_exists {
-        return None;
-    }
-
-    let mut latest_time = None;
-    for entry in dir.read_dir().expect("Failed to open save directory") {
-        if let Ok(entry) = entry {
-            let file_name = entry.file_name();
-            if file_name
-                .to_str()
-                .unwrap_or_default()
-                .starts_with(&dir.file_name().unwrap().to_str().unwrap())
-            {
-                let metadata = entry.metadata();
-                if let Ok(metadata) = metadata {
-                    let modified = metadata.modified().expect("System not supported");
-                    println!("{:?}", modified);
-                    if latest_time == None || modified > latest_time.unwrap() {
-                        latest_time = Some(modified);
-                    }
-                }
-            }
+pub fn get_backup_state(config: &GameConfig) -> Result<BackupState, io::Error> {
+    // Check save files
+    let mut last_modified_time = SystemTime::UNIX_EPOCH;
+    for file in config.file_list.into_iter() {
+        let last_modified = file.metadata()?.modified()?;
+        if last_modified > last_modified_time {
+            last_modified_time = last_modified;
         }
     }
 
-    latest_time
+    let mut latest_backup_time = SystemTime::UNIX_EPOCH;
+    let mut oldest_backup_path: Option<PathBuf> = None;
+    let mut oldest_backup_time = SystemTime::now();
+    for file in config
+        .save_dir
+        .read_dir()?
+        .filter_map(|x| x.ok())
+        .filter(|x| {
+            x.to_owned()
+                .file_name()
+                .as_os_str()
+                .to_str()
+                .unwrap()
+                .to_owned()
+                .contains(&config.name)
+        })
+    {
+        let created_time = file.metadata()?.created()?;
+        if created_time < oldest_backup_time {
+            oldest_backup_path = Some(file.path());
+            oldest_backup_time = created_time;
+        }
+        if created_time > latest_backup_time {
+            latest_backup_time = created_time;
+        }
+    }
+    Ok(BackupState::new(
+        last_modified_time,
+        latest_backup_time,
+        oldest_backup_path,
+    ))
 }
 
 #[cfg(test)]
 mod test {
-    use std::path::Path;
+    use std::{fs, io, path::PathBuf};
 
-    use super::get_latest_backup_time;
+    use crate::{
+        backup::backup_types::BackupState,
+        config::config_types::{FileList, GameConfig},
+    };
+
+    use super::get_backup_state;
 
     #[test]
-    fn test_latest_time() {
-        let latest = get_latest_backup_time(Path::new("./test/test_latest/file"));
+    fn test_backup_state() -> Result<(), io::Error> {
+        let config = GameConfig {
+            name: "file".to_owned(),
+            save_dir: PathBuf::from("./test/test_state/save"),
+            zip: false,
+            file_list: FileList::new("./test/test_state/root", None, None),
+            interval: 30,
+            count: 10,
+        };
+        let state = get_backup_state(&config)?;
+        let last_modified_time = fs::metadata("test/test_state/root/file2")?.modified()?;
+        let latest_backup_time = fs::metadata("test/test_state/save/file2")?.created()?;
+        let oldest_backup_path = Some(PathBuf::from(r"./test/test_state/save\file1"));
+        assert_eq!(
+            state,
+            BackupState::new(last_modified_time, latest_backup_time, oldest_backup_path)
+        );
+        Ok(())
     }
 }
