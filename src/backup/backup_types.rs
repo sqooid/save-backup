9863e2e08c1ae9a40fdb5caf2992addddf0065b1
Lucas Liu <lucasliu7@hotmail.com>
2022-04-14 18:46:33 +1000

    restructured tests

--- src/backup/backup_types.rs
+++ src/backup/backup_types.rs
@@ -1,7 +1,22 @@
 use std::{path::PathBuf, time::SystemTime};
 
+#[derive(Debug, PartialEq)]
 pub struct BackupState {
-    last_modified_time: SystemTime,
-    latest_backup_time: SystemTime,
-    oldest_backup_path: Option<PathBuf>,
+    pub last_modified_time: SystemTime,
+    pub latest_backup_time: SystemTime,
+    pub oldest_backup_path: Option<PathBuf>,
+}
+
+impl BackupState {
+    pub fn new(
+        last_modified_time: SystemTime,
+        latest_backup_time: SystemTime,
+        oldest_backup_path: Option<PathBuf>,
+    ) -> Self {
+        Self {
+            last_modified_time,
+            latest_backup_time,
+            oldest_backup_path,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,4 @@ mod utils {
 fn main() {
     let configs = config::parse::read_config_from_file("test-1.yaml");
     println!("{:?}", configs);
-    let latest = backup::file_data::get_latest_backup_time(Path::new("./test/test_latest/file"));
 }
